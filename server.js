f6ac699ca3b6cabfc12e1259851f5a968554787b
sakme <alan@akme.us>
2022-01-16 09:48:37 -0500

    connected to sql

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,12 +7,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Akme@9d9d',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`select * from candidates`, (err, rows) => {
+    console.log(rows);
 });
 
+// 404 error
 app.use((req, res) => {
     res.status(404).end();
 })
