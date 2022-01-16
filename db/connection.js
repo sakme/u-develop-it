58f157d91c353cb51d78a6bcb549586c7d862f52
sakme <alan@akme.us>
2022-01-16 15:16:19 -0500

    removed password

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'Akme@9d9d',
+        password: '',
         database: 'election'
     },
     console.log('Connected to the election database.')
